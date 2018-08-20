afc496a86f9fab405c0f1da6ac6113965de8b9b9
FoxKat <kfox3151@gmail.com>
2018-08-20 14:00:04 +0100

    Adding child routes to the product.module for product edit info and tags components.

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -4,6 +4,8 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit.component';
+import { ProductEditInfoComponent } from './product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit-tags.component';
 
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductService } from './product.service';
@@ -22,7 +24,22 @@ import { SharedModule } from '../shared/shared.module';
       },
       { path: 'products/:id/edit',
         component: ProductEditComponent,
-        resolve: { product: ProductResolver }
+        resolve: { product: ProductResolver },
+        children: [
+        {
+          path: '',
+          redirectTo: 'info',
+          pathMatch: 'full'
+        },
+        {
+          path: 'info',
+          component: ProductEditInfoComponent
+        },
+        {
+          path: 'tags',
+          component: ProductEditTagsComponent
+        }
+      ]
       }
     ])
   ],
@@ -30,7 +47,9 @@ import { SharedModule } from '../shared/shared.module';
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
-    ProductFilterPipe
+    ProductFilterPipe,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ],
   providers: [
     ProductService,
