6cae1bc386607f6f7da13c938a85b9ddead7aa9a
FoxKat <kfox3151@gmail.com>
2018-08-21 10:48:26 +0100

    Removing Product Module from app module in preperation for lazy loading.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { PageNotFoundComponent } from './page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
 
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 
@@ -21,7 +20,6 @@ import { MessageModule } from './messages/message.module';
     BrowserModule,
     HttpModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    ProductModule,
     UserModule,
     MessageModule,
     AppRoutingModule
